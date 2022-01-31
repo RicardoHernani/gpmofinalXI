8a2b5cc5cee251bbbbdf9b0f41800c85a4ac051c
RicardoHernani <ricardohernani@yahoo.com.br>
2022-01-30 23:50:36 -0300

    Criando perfil test e dev

---
+++ src/main/java/com/ricardochaves/config/DevConfig.java
@@ -0,0 +1,41 @@
+package com.ricardochaves.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.ricardochaves.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+	
+	@Autowired
+	private DBService dbService;
+		
+	@Value("${spring.jpa.hibernate.ddl-auto}")   			
+	private String strategy1;	
+	
+	@Value("${spring.jpa.defer-datasource-initialization}")   			
+	private String strategy2;	
+	
+	@Value("${spring.sql.init.mode}")   			
+	private String strategy3;	
+
+	
+	//para criar o banco em toda inicialização deve-se usar create, true e always
+	//para manter os dados deve-se usar none, false e never
+		
+	@Bean
+	public boolean instantiateDatabase() {   
+		
+		if(!"create".equals(strategy1) && (!"true".equals(strategy2) && (!"always".equals(strategy3)))) {
+			return false;
+		}
+		
+		dbService.instantiateTestDatabase();
+		return true;
+	}
+}
---
+++ src/main/java/com/ricardochaves/services/DBService.java
@@ -0,0 +1,12 @@
+package com.ricardochaves.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DBService {
+	
+	public void instantiateTestDatabase() {
+		
+	}
+
+}
